cc786c049a485e6501f639ac16e742f57c850cdb
Jhonatas Correa <Jhonatasrcorrea@gmail.com>
2023-07-13 23:07:03 -0300

    Criação da entidade cliente (na camada domain)

---
+++ GymClientControl.Domain/Entities/v1/Client/Client.cs
@@ -0,0 +1,34 @@
+﻿using GymClientControl.Domain.Enums.v1;
+
+namespace GymClientControl.Domain.Entities.v1.Client
+{
+    public class Client
+    {
+        public Client(string name, 
+                      DateTime dateBirth, 
+                      string phone, 
+                      string document, 
+                      string email, 
+                      bool active, 
+                      DateTime registrationDate)
+        {
+            Name = name;
+            DateBirth = dateBirth;
+            Phone = phone;
+            Document = document;
+            Email = email;
+            Active = active;
+            RegistrationDate = registrationDate;
+        }
+
+        public string Name { get; private set; } 
+        public DateTime DateBirth { get; private set; }
+        public string Phone { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public bool Active { get; private set; }
+        public ContractTime ContractTime { get; private set; }
+        public DateTime RegistrationDate { get; private set; }
+        public string Adress { get; private set; }
+    }
+}
---
+++ GymClientControl.Domain/Enums/v1/ContractTime.cs
@@ -0,0 +1,9 @@
+﻿namespace GymClientControl.Domain.Enums.v1
+{
+    public enum ContractTime
+    {
+        Monthly = 0,
+        Quarterly = 1,
+        Semester = 2
+    }
+}
